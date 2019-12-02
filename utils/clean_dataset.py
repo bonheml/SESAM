5fbaf2e1defc5ce189138f1dd9111a20110291bf
Lisa Bonheme <lb732@kent.ac.uk>
2019-12-02 14:07:26 +0000

    Fix DS cleaning
    regex for quotes removal caused some entries to be erased completely. Now all double quotes are deleted instead.

--- utils/clean_dataset.py
+++ utils/clean_dataset.py
@@ -5,19 +5,6 @@ logger = logging.getLogger("dataset_cleaner")
 logger.setLevel(logging.INFO)
 
 special_quotes = re.compile(r"[＂〃ˮײ᳓″״‶˶ʺ“”˝‟]+", re.M)
-unclosed_quotes = re.compile(r"(,([^\",]|(\"[^\",]*\"))*)(\")([^\",]*)(?=,)", re.M)
-
-
-def remove_unclosed_quotes(content):
-    m = unclosed_quotes.findall(content, re.M)
-    logger.info("{} unclosed quotes found".format(len(m)))
-    logger.debug(m)
-
-    quotes_cleaned = re.sub(unclosed_quotes, r"\1\3", content)
-    m = unclosed_quotes.findall(quotes_cleaned, re.M)
-    logger.info("{} unclosed quotes found after replacement".format(len(m)))
-    logger.debug(m)
-    return quotes_cleaned
 
 
 def replace_special_quotes(content):
@@ -25,7 +12,7 @@ def replace_special_quotes(content):
     logger.info("{} special quotes found".format(len(m)))
     logger.debug(m)
 
-    quotes_replaced = re.sub(special_quotes, "\"", content)
+    quotes_replaced = re.sub(special_quotes, "", content)
     m = special_quotes.findall(quotes_replaced, re.M)
     logger.info("{} special quotes found after replacement".format(len(m)))
     logger.debug(m)
@@ -35,7 +22,6 @@ def replace_special_quotes(content):
 def cleanup_dataset(input_file, output_file):
     with open(input_file) as f_in:
         content = f_in.read()
-    content_special_quotes_removed = replace_special_quotes(content)
-    content_cleaned = remove_unclosed_quotes(content_special_quotes_removed)
+    content_cleaned = replace_special_quotes(content)
     with open(output_file, "w+") as f_out:
         f_out.write(content_cleaned)
