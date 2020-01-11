be0c30c9f0a0a566234853ddaa3d1d886fb558a5
Lisa Bonheme <lb732@kent.ac.uk>
2020-01-11 13:16:23 +0000

    Add comments to CCA code

--- utils/cca.py
+++ utils/cca.py
@@ -5,9 +5,12 @@ from scipy import linalg
 def cross_covariance(x, y):
     """ Compute the cross covariance matrix
 
-    :param x: the first matrix
-    :param y: the second matrix
+    :param x: the first matrix of size T x N
+    :type x: np.array
+    :param y: the second matrix of size T x M
+    :type y: np.array
     :return: variance of x, covariance of (x,y), variance of y
+    :rtype: tuple
     """
     n = x.shape[0]
 
@@ -23,7 +26,9 @@ def matrix_sqrt_inv(x):
     """ Compute the inverse square root of a matrix
 
     :param x: the matrix to use
+    :type x: np.array
     :return: the inverse square root of the matrix (x^-1/2)
+    :rtype: np.array
     """
     # Add small value for a more stable computation as done in https://github.com/google/svcca/blob/master/cca_core.py
     x += np.finfo(x.dtype).eps * np.eye(x.shape[0])
@@ -54,10 +59,13 @@ def corr_coef(x, y):
 def compute_cca(x, y):
     """ Compute canonical correlation analysis of matrices x and y using SVD
 
-    :param x: the matrix X
-    :param y: the matrix Y
+    :param x: the matrix X of size T x N
+    :type x: np.array
+    :param y: the matrix Y of size T x M
+    :type y: np.array
     :return: a dictionary containing svd values, the coefficients w_x and w_y, the canonical loadings and cross loadings,
     the canonical variates of x and y (z_x and z_y) and the diagonal matrix of correlations
+    :rtype: dict
     """
     res = {}
 
@@ -65,10 +73,12 @@ def compute_cca(x, y):
     x = (x - x.mean(axis=0))
     y = (y - y.mean(axis=0))
 
-    c_xx, c_xy, c_yy = cross_covariance(x, y)  # Compute cross covariance matrix
+    # Compute cross covariance matrix
+    c_xx, c_xy, c_yy = cross_covariance(x, y)
 
-    c_xx_sqrt_inv = matrix_sqrt_inv(c_xx)  # Compute c_xx^-1/2
-    c_yy_sqrt_inv = matrix_sqrt_inv(c_yy)  # Compute c_yy^-1/2
+    #  Compute c_xx^-1/2 and c_yy^-1/2
+    c_xx_sqrt_inv = matrix_sqrt_inv(c_xx)
+    c_yy_sqrt_inv = matrix_sqrt_inv(c_yy)
 
     # Doing SVD decomposition for c_xx^-1/2 * c_xy * c_yy^-1/2
     to_decompose = np.matmul(c_xx_sqrt_inv, np.matmul(c_xy, c_yy_sqrt_inv))
@@ -77,6 +87,7 @@ def compute_cca(x, y):
     res["svd"] = (u, s, v)
     res["sigma"] = s
 
+    # Reduce the dimensionality of U and S to the number of correlations coefficients found
     dim_to_keep = range(0, len(s))
     u = u[:, dim_to_keep]
     v = v[:, dim_to_keep]
