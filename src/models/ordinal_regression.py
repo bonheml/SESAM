6b3d30434161e11e00dbbf6a60b8d46b13e82949
Lisa Bonheme <lb732@kent.ac.uk>
2020-02-28 17:11:28 +0000

    Make ordinal regression fully compatible with sklearn
    Now it can be used as any other sklearn model

--- src/models/ordinal_regression.py
+++ src/models/ordinal_regression.py
@@ -1,8 +1,12 @@
-from sklearn.base import clone
+from sklearn.base import clone, ClassifierMixin, MetaEstimatorMixin, BaseEstimator
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.utils import check_X_y
+from sklearn.utils.multiclass import check_classification_targets
+from sklearn.utils.validation import check_is_fitted, check_array
 
 
-class OrdinalClassifier:
+class OrdinalClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
     """ This ordinal classifier uses the approach proposed by [1].
     It should not be used when the number of ordinal labels is large as the results are more noisy.
     The implementation is a modified version of the one proposed by [2].
@@ -11,38 +15,66 @@ class OrdinalClassifier:
     In European Conference on Machine Learning (pp. 145-156). Springer, Berlin, Heidelberg.
     [2] https://towardsdatascience.com/simple-trick-to-train-an-ordinal-regression-with-any-classifier-6911183d2a3c
     """
-    def __init__(self, clf, labels):
+    def __init__(self, estimator=LogisticRegression()):
         """Initialise the wrapper with the classifier to use and the labels
 
-        :param clf: Which classifier to use. Can be any sklearn binary classifier
-        :param labels: labels used for the classification
+        :param estimator: Which classifier to use. Can be any sklearn binary classifier
         """
-        self.clfs = {i: clone(clf) for i in range(len(labels) - 1)}
-        self.labels = labels
+        self.estimator = estimator
 
-    def _check_y_labels(self, y):
-        """Ensure that the given labels match the ones provided during initialisation
+    def _more_tags(self):
+        return {'multioutput_only': True}
 
-        :param y: list of labels
-        :return: None
-        """
-        y_labels = np.unique(y)
-        try:
-            assert set(y_labels) == set(self.labels)
-        except AssertionError:
-            raise AssertionError("The labels {} are not matching the given classes {}".format(y_labels, self.labels))
+    def get_params(self, deep=True):
+        return {"estimator": self.estimator}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
+
+    def _fit_multi(self):
+        classes_list = list(self.classes_)
+        for i in range(self.classes_.shape[0] - 1):
+            clf = clone(self.estimator)
+            binary_y = np.array(list(map(lambda x: classes_list.index(x) > i, self.y_)))
+            clf.fit(self.X_, binary_y)
+            self.estimators_[self.classes_[i]] = clf
 
     def fit(self, X, y):
-        """Fit the n_labels -1 classifiers
+        """ Fit the n_labels -1 classifiers
 
-        :param X: input features
-        :param y: true labels
-        :return: None
+        :param X  array-like, shape (n_samples, n_features) The training input samples.
+        :param y: array-like, shape (n_samples,) The target values. An array of int.
+        :return: Returns self.
+        :rtype: object
         """
-        self._check_y_labels(y)
-        for k, clf in self.clfs.items():
-            binary_y = list(y.map(lambda x: self.labels.index(x) > k))
-            clf.fit(X, binary_y)
+        X, y = check_X_y(X, y)
+        check_classification_targets(y)
+        self.y_ = y
+        self.X_ = X
+
+        self.classes_, y = np.unique(y, return_inverse=True)
+        self.estimators_ = {}
+
+        if len(self.classes_) == 1:
+            self.estimator.fit(self.X_, self.y_)
+        else:
+            self._fit_multi()
+
+        return self
+
+    def _predict_multi(self, X):
+        clfs_pred = {i: clf.predict_proba(X) for i, clf in self.estimators_.items()}
+        n_classes = self.classes_.shape[0] - 1
+        predicted_proba = []
+
+        for i, name in enumerate(self.classes_):
+            prev_pred = 1 if i == 0 else clfs_pred[self.classes_[i - 1]][:, 1]
+            curr_pred = 0 if i >= n_classes else clfs_pred[self.classes_[i]][:, 1]
+            predicted_proba.append(prev_pred - curr_pred)
+
+        return predicted_proba
 
     def predict_proba(self, X):
         """Predict the probability of each ordinal label
@@ -50,14 +82,13 @@ class OrdinalClassifier:
         :param X: input features
         :return: list of predicted probabilities
         """
-        clfs_pred = {i: clf.predict_proba(X) for i, clf in self.clfs.items()}
-        n_classes = len(self.labels) - 1
-        predicted_proba = []
+        check_is_fitted(self, ['X_', 'y_'])
+        X = check_array(X)
 
-        for i, name in enumerate(self.labels):
-            prev_pred = 1 if i == 0 else clfs_pred[i - 1][:, 1]
-            curr_pred = 0 if i >= n_classes else clfs_pred[i][:, 1]
-            predicted_proba.append(prev_pred - curr_pred)
+        if len(self.classes_) == 1:
+            predicted_proba = self.estimator.predict_proba(X)
+        else:
+            predicted_proba = self._predict_multi(X)
 
         return np.vstack(predicted_proba).T
 
@@ -67,4 +98,8 @@ class OrdinalClassifier:
         :param X: input features
         :return: the predicted label
         """
-        return np.argmax(self.predict_proba(X), axis=1)
+        check_is_fitted(self, ['X_', 'y_'])
+        if len(self.classes_) == 1:
+            return self.estimator.predict(X)
+        D = self.predict_proba(X)
+        return self.classes_[np.argmax(D, axis=1)]
