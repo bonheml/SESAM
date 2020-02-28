6c263cc3034713ba0487d15322a92e682d7ffb97
Lisa Bonheme <lb732@kent.ac.uk>
2020-02-28 20:08:42 +0000

    Add report generation
    Generate zip file following competition guidelines

---
+++ src/utils/reports.py
@@ -0,0 +1,20 @@
+from zipfile import ZipFile
+import numpy as np
+
+
+def generate_report(task_a, task_b, task_c, save=True):
+    report = ""
+    task_a = task_a - 1
+    for i in range(task_a.shape[0]):
+        task_b_str = np.array2string(task_b[i], separator="")[1:-1]
+        task_c_str = np.array2string(task_c[i], separator="")[1:-1]
+        report += "{}_{}_{}\n".format(task_a[i], task_b_str, task_c_str)
+
+    if save is True:
+        print(report, file=open("answer.txt", 'w'))
+        with ZipFile("res.zip", 'w') as myzip:
+            myzip.write("answer.txt")
+
+    return report
+
+
