76541771300d24feeef3d89d0dd962ff1f5fb3b8
Lisa Bonheme <lb732@kent.ac.uk>
2020-03-12 19:43:26 +0000

    Use DCCA embeddings for models training
    - update embeddings loader to use DCCA embeddings as well

--- src/utils/embeddings.py
+++ src/utils/embeddings.py
@@ -31,17 +31,19 @@ def load_sent_embeddings(source_file):
     return sent_embed
 
 
-def load_embeds(text_embed, img_embed):
+def load_embeds(text_embed, img_embed, dcca_embed):
     """ Load image and sentence embeddings and create a concatenated version
 
     :param text_embed: pickle file containing the sentence embeddings
     :param img_embed: pickle file containing the image embeddings
+    :param dcca_embed: pickle file containing the deep cca embeddings
     :return: tuple containing sentence embeddings, image embeddings, concatenated embeddings
     """
     img_embed = load_img_embeddings(img_embed)
     sent_embed = load_sent_embeddings(text_embed)
+    dcca_embed = load_pickle_file(dcca_embed)
     concat = np.concatenate((sent_embed, img_embed), axis=1)
-    return sent_embed, img_embed, concat
+    return sent_embed, img_embed, dcca_embed, concat
 
 
 def retrieve_all_embeds(to_retrieve):
@@ -54,12 +56,14 @@ def retrieve_all_embeds(to_retrieve):
     """
     embed = {"image only": [],
              "text only": [],
+             "deep cca": [],
              "concatenated": []}
 
-    for sent_embed_file, img_embed_file in to_retrieve:
-        sent_embed, img_embed, concat_embed = load_embeds(sent_embed_file, img_embed_file)
+    for sent_embed_file, img_embed_file, dcca_embed_file in to_retrieve:
+        sent_embed, img_embed, dcca_embed, concat_embed = load_embeds(sent_embed_file, img_embed_file, dcca_embed_file)
         embed["image only"].append(img_embed)
         embed["text only"].append(sent_embed)
+        embed["deep cca"].append(dcca_embed)
         embed["concatenated"].append(concat_embed)
 
     return embed
